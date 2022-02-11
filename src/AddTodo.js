b1b5f6bf0ed13eec8c0247c6cc3a131fdf38ac6a
Sahil Prajapati <sprajapati14012002@gmail.com>
2022-02-11 21:48:18 +0530

    lengthError and press enter key updated

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,15 +1,40 @@
 import {useState} from 'react';
 const AddTodo = ({todos,setTodos,remtask,setRemtask})=>{
+    const [lengthError, setLengthError] = useState("");
+    const handleKeyPress = (event) => {
+        if(event.key === 'Enter' || event.key === 'NumpadEnter'){
+          if(content.length >= 5){
+            addTodo(content);
+          }else{
+            setLengthError('Todo task length must be greater than or equal to 5');
+          }
+        }
+    }
     const addTodo = (content) => {
-        const id = todos.length+1;
-        let newTodos = [...todos,{id:id,content:content}]
-        setTodos(newTodos);
-        setContent('')
+        if(content.length>=5){
+            const id = todos.length+1;
+            let newTodos = [...todos,{id:id,content:content}]
+            setTodos(newTodos);
+            setContent('')
+        }else{
+            setLengthError('Todo task length must be greater than or equal to 5');
+        }
+        
     }
     const [content,setContent]=useState('')
     return (
         <div className="add">
-            <input type="text" placeholder="Add New Todo" value={content} onChange={(e)=>setContent(e.target.value)} />
+        <input type="text" placeholder="Add New Todo" value={content} onChange={(e)=>{
+            if(e.target.value.length>=5){
+                console.log(e.target.value)
+                setLengthError('')}
+                setContent(e.target.value)}
+                } onKeyPress={handleKeyPress} />
+            {lengthError.length > 0 && (
+            <span style={{color: "red",fontSize: "12px",marginLeft: "10px",}}>
+              {lengthError}
+            </span>
+            )}
             <button onClick={()=>addTodo(content)}>Submit</button>
             <button onClick={()=>setRemtask(!remtask)}>Remove Completed Task</button>
         </div>
